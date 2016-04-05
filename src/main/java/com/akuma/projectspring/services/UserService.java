d1013e61064d6b15835f22adaf64ea5e15ec6c74
Sagar <sthapa7@mail.ccsf.edu>
2016-04-05 16:31:57 -0500

    comment

--- src/main/java/com/akuma/projectspring/services/UserService.java
+++ src/main/java/com/akuma/projectspring/services/UserService.java
@@ -2,7 +2,11 @@ package com.akuma.projectspring.services;
 
 import com.akuma.projectspring.models.Users;
 
-
+/**
+ * 
+ * @author Boot Camp User 06
+ *
+ */
 public interface UserService {
 	
 	public boolean isValid(Users user);
